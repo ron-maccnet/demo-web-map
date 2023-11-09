3c205e132aefa96019a4454e4ef7d17682f77ace
Ron Bentley <rbentley@maccnet.com>
2023-11-08 20:22:42 -0600

    Add deployment script.

---
+++ deploy_demo.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+#
+# Deploy a tiny demo web app for MACC testing.
+
+if [ -z "$1" ]
+  then echo "Usage: deploy_demo [hostname]"
+  exit
+fi
+
+echo
+echo "Installing the webserver, NGINX."
+sudo apt install nginx
+
+echo
+echo "Installing certbot for SSL certs with Let's Encrypt."
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+echo
+echo "Modifying the web server host name to $1."
+sed "s/server_name _/server_name $1/" /etc/nginx/sites-available/default
+sudo systemctl reload nginx
+
+echo
+echo "Obtaining an SSL cert for $1 from Let's Encrypt."
+sudo certbot --nginx -d $1
